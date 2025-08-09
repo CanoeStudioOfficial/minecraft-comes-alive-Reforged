3b2d61730abddbbcf8c8f6a843abe4470af97ff3
Lonelyxiya <3438505487@qq.com>
2025-08-09 14:38:04 +0800

    what 6.2.0 fxxk

--- src/main/java/mca/api/exceptions/VillagerSpawnException.java
+++
@@ -1,7 +0,0 @@
-package mca.api.exceptions;
-
-public class VillagerSpawnException extends Exception {
-	public VillagerSpawnException(String message) {
-		super(message);
-	}
-}
--- src/main/java/mca/items/ItemSpecialCaseGift.java
+++
@@ -1,9 +0,0 @@
-package mca.items;
-
-import mca.api.objects.Player;
-import mca.entity.EntityVillagerMCA;
-import net.minecraft.item.Item;
-
-public abstract class ItemSpecialCaseGift extends Item {
-    public abstract boolean handle(Player player, EntityVillagerMCA villager);
-}
