d68598fd95e009750c511a620cfbae78c76121b2
Lonelyxiya <3438505487@qq.com>
2025-08-15 16:08:52 +0800

    clean code

--- src/main/java/mca/core/MCA.java
+++ src/main/java/mca/core/MCA.java
@@ -52,7 +52,7 @@ public class MCA {
     private static Logger logger;
     private static Localizer localizer;
     private static Config config;
-    public String[] supporters = new String[0];
+
 
     public static Logger getLog() {
         return logger;
@@ -117,11 +117,9 @@ public class MCA {
 
 
     public String getRandomSupporter() {
-        if (supporters.length > 0) {
-            return supporters[new Random().nextInt(supporters.length)];
-        } else {
+
             return API.getRandomName(EnumGender.getRandom());
-        }
+
     }
 
 }
